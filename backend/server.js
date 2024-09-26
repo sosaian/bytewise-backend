4369dbddfe3d5da8647235f596b629fffdf9219e
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-09-26 13:42:54 -0300

    Move server.js to root directory in backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import app from './src/app.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.NODE_SERVER_PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
