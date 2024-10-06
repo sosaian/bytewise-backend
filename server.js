4aa7d37e70efdc7b9518442b94ce3a4686ff4643
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-06 17:52:02 -0300

    Adapt server.js to new config.js .env management

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 import app from './src/app.js'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-
-const PORT = process.env.NODE_SERVER_PORT || 3000
+import { PORT } from "./config.js"
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
-
