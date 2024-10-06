62852212d2f5b636578c7ff51c5133258b278cc9
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-06 18:41:48 -0300

    Refactor updateTask() to dinamically create the query from recieved data

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,29 +1,47 @@
-import pool from '../db/db.js';
+import pool from '../db/db.js'
 
 class Task {
     static async createTask(id_user, description_task, status_task, due_date) {
         const [result] = await pool.query(
             'INSERT INTO task (id_user, description_task, status_task, due_date) VALUES (?, ?, ?, ?)',
             [id_user, description_task, status_task, due_date]
-        );
-        return result.insertId;
+        )
+        return result.insertId
     }
 
     static async getTaskById(id) {
-        const [rows] = await pool.query('SELECT * FROM task WHERE id = ?', [id]);
-        return rows[0];
+        const [rows] = await pool.query('SELECT * FROM task WHERE id = ?', [id])
+        return rows[0]
     }
 
-    static async updateTask(id, description_task, status_task, due_date) {
-        await pool.query(
-            'UPDATE task SET description_task = ?, status_task = ?, due_date = ? WHERE id = ?',
-            [description_task, status_task, due_date, id]
-        );
+    static async updateTask(id, data) {
+        let query = `UPDATE task SET`
+        const params = []
+
+        if (data.description_task) {
+            query += ' description_task = ?,'
+            params.push(data.description_task)
+        }
+
+        if (data.status_task) {
+            query += ' status_task = ?,'
+            params.push(data.status_task)
+        }
+
+        if (data.due_date) {
+            query += ' due_date = ?,'
+            params.push(data.due_date)
+        }
+        
+        query = query.slice(0, -1) + ' WHERE id = ?'
+        params.push(id)
+
+        await pool.query(query, params)
     }
 
     static async deleteTask(id) {
-        await pool.query('DELETE FROM task WHERE id = ?', [id]);
+        await pool.query('DELETE FROM task WHERE id = ?', [id])
     }
 }
 
-export default Task;
+export default Task
