e585018e05cfd99f23333b7c176c72c991ff18a5
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-06 19:35:51 -0300

    Change export method in transactionController.js adapting transactionRoutes.js alongside it

--- src/controller/transactionController.js
+++ src/controller/transactionController.js
@@ -1,70 +1,68 @@
-import Transaction from '../models/Transaction.js';
+import Transaction from '../models/Transaction.js'
 
-class TransactionController {
+export class TransactionController {
     static async create(req, res) {
-        const { id_user, type_transaction, amount, description_transaction} = req.body;
+        const { id_user, type_transaction, amount, description_transaction} = req.body
         try {
-            const transactionId = await Transaction.createTransaction(id_user, type_transaction, amount, description_transaction);
-            res.status(201).json({ id: transactionId });
+            const transactionId = await Transaction.createTransaction(id_user, type_transaction, amount, description_transaction)
+            res.status(201).json({ id: transactionId })
         } catch (error) {
-            res.status(500).json({ error: 'Error creating transaction' });
+            res.status(500).json({ error: 'Error creating transaction' })
         }
     }
 
     static async get(req, res) {
-        const { id } = req.params;
+        const { id } = req.params
         try {
-            const transaction = await Transaction.getTransactionById(id);
+            const transaction = await Transaction.getTransactionById(id)
             if (transaction) {
-                res.json(transaction);
+                res.json(transaction)
             } else {
-                res.status(404).json({ error: 'Transaction not found' });
+                res.status(404).json({ error: 'Transaction not found' })
             }
         } catch (error) {
-            res.status(500).json({ error: 'Error fetching transaction' });
+            res.status(500).json({ error: 'Error fetching transaction' })
         }
     }
 
     static async update(req, res) {
-        const { id } = req.params;
-        const { type_transaction, amount, date_transaction, description_transaction } = req.body;
+        const { id } = req.params
+        const { type_transaction, amount, date_transaction, description_transaction } = req.body
     
         // Crear un objeto con los campos opcionales que se quieren actualizar
-        const data = {};
-        if (type_transaction) data.type_transaction = type_transaction;
-        if (amount) data.amount = amount;
-        if (date_transaction) data.date_transaction = date_transaction;
-        if(description_transaction) data.description_transaction = description_transaction;
+        const data = {}
+        if (type_transaction) data.type_transaction = type_transaction
+        if (amount) data.amount = amount
+        if (date_transaction) data.date_transaction = date_transaction
+        if(description_transaction) data.description_transaction = description_transaction
     
         try {
-            await Transaction.updateTransaction(id, data);
-            res.json({ message: 'Transaction updated successfully' });
+            await Transaction.updateTransaction(id, data)
+            res.json({ message: 'Transaction updated successfully' })
         } catch (error) {
-            res.status(500).json({ error: 'Error updating transaction' });
+            res.status(500).json({ error: 'Error updating transaction' })
         }
     }
 
     static async delete(req, res) {
-        const { id } = req.params;
+        const { id } = req.params
         try {
-            await Transaction.deleteTransaction(id);
-            res.json({ message: 'Transaction deleted successfully' });
+            await Transaction.deleteTransaction(id)
+            res.json({ message: 'Transaction deleted successfully' })
         } catch (error) {
-            res.status(500).json({ error: 'Error deleting transaction' });
+            res.status(500).json({ error: 'Error deleting transaction' })
         }
     }
 
 
     // Obtener resumen del presupuesto
     static async getBudgetSummary(req, res) {
-        const { id_user } = req.params; // Se asume que el id_user se pasa como parámetro
+        const { id_user } = req.params // Se asume que el id_user se pasa como parámetro
         try {
-            const budgetSummary = await Transaction.getBudgetSummary(id_user);
-            res.json(budgetSummary);
+            const budgetSummary = await Transaction.getBudgetSummary(id_user)
+            res.json(budgetSummary)
         } catch (error) {
-            res.status(500).json({ error: 'Error fetching budget summary', error });
+            res.status(500).json({ error: 'Error fetching budget summary', error })
         }
     }
 }
-
-export default TransactionController;
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import authMiddleware  from '../middleware/authMiddleware.js'
-import TransactionController from '../controller/transactionController.js'
+import { TransactionController } from '../controller/transactionController.js'
 
 const router = express.Router()
 
