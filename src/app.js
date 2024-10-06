5bda73e5708051a87d4cdca1bd0f61754952bdfd
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-06 18:13:51 -0300

    Remove routes.js import in app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,19 @@
 import express from 'express'
 import cors from 'cors'
-import routes from './routes/routes.js'
 import userRoutes from './routes/userRoutes.js'
 import transactionRoutes from './routes/transactionRoutes.js'
 import taskRoutes from './routes/taskRoutes.js'
+
 const app = express()
 
 app.use(cors())
 
 app.use(express.json())
 
-app.use('/', routes)
-
 app.use(userRoutes)
 
 app.use(transactionRoutes)
 
 app.use(taskRoutes)
-
 
 export default app
