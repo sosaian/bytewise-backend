24ce6c321678a9cd59fd902fcec68c40165fa68e
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-06 18:45:02 -0300

    Add centralized .env management into db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,30 +1,29 @@
-import dotenv from 'dotenv'
 import mysql from 'mysql2'
-dotenv.config()
+import { mysql_host, mysql_user, mysql_password, mysql_database } from '../../config.js'
 
 // Crea la conexión 
 const pool = mysql.createPool({
-    host: process.env.mysql_host,
-    user: process.env.mysql_user,
-    password: process.env.mysql_password,
-    database: process.env.mysql_database
+    host: mysql_host,
+    user: mysql_user,
+    password: mysql_password,
+    database: mysql_database
 }).promise()
 
 // Manejo de la conexion / errores
 try {
-    await pool.getConnection(); // Intenta obtener una conexión para verificar la configuración
-    console.log('Conexión a la base de datos establecida correctamente.');
+    await pool.getConnection() // Intenta obtener una conexión para verificar la configuración
+    console.log('Conexión a la base de datos establecida correctamente.')
 } catch (error) {
-    console.error('Error conectando a la base de datos:', error);
-    process.exit(1); // Salir si no se puede conectar
+    console.error('Error conectando a la base de datos:', error)
+    process.exit(1) // Salir si no se puede conectar
 }
 
 
 // Cerrar el pool al terminar la aplicación
 process.on('SIGINT', async () => {
-    await pool.end();
-    console.log('Pool de conexiones cerrado.');
-    process.exit(0);
-});
+    await pool.end()
+    console.log('Pool de conexiones cerrado.')
+    process.exit(0)
+})
 
 export default pool
