2d94c5a6d1bef7dff02195879e9eefd0eb88710a
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-06 18:31:32 -0300

    Add getUserByEmail() method to User class in User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,5 @@
 //Se importa ya que pool es un objeto que permite realizar consultas a la base de datos
-import pool from '../db/db.js';
-
-
+import pool from '../db/db.js'
 
 class User{
 
@@ -11,52 +9,59 @@ class User{
         const [result] = await pool.query(
             'insert into user (name_user, email, password_user) values (?,?,?)',
             [name_user, email, password_user]
-        );
+        )
         //Devuelve id del nuevo usuario creado.
-        return result.insertId;
+        return result.insertId
     }
 
 
     static async getUserById(id){
         const [rows] = await pool.query (
             'select * from user where id = ?',
-            [id]);
+            [id])
 
-        return rows[0];
+        return rows[0]
+    }
+
+    static async getUserByEmail(email){
+        const [rows] = await pool.query (
+            'select * from user where email = ?',
+            [email])
+
+        return rows[0]
     }
 
     static async updateUser(id, data) {
-        let query = 'UPDATE user SET';
-        const params = [];
+        let query = 'UPDATE user SET'
+        const params = []
     
         // Construir dinámicamente la consulta solo con los campos proporcionados
         if (data.name_user) {
-            query += ' name_user = ?,';
-            params.push(data.name_user);
+            query += ' name_user = ?,'
+            params.push(data.name_user)
         }
         if (data.email) {
-            query += ' email = ?,';
-            params.push(data.email);
+            query += ' email = ?,'
+            params.push(data.email)
         }
         if (data.password_user) {
-            query += ' password_user = ?,';
-            params.push(data.password_user);
+            query += ' password_user = ?,'
+            params.push(data.password_user)
         }
     
         // Eliminar la última coma y añadir la condición WHERE
-        query = query.slice(0, -1) + ' WHERE id = ?';
-        params.push(id);
+        query = query.slice(0, -1) + ' WHERE id = ?'
+        params.push(id)
     
         // Ejecutar la consulta con los parámetros
-        await pool.query(query, params);
+        await pool.query(query, params)
     }
     
 
    
     static async deleteUser(id) {
-        await pool.query('DELETE FROM user WHERE id = ?', [id]);
+        await pool.query('DELETE FROM user WHERE id = ?', [id])
     }
 }
 
-export default User;
-
+export default User
