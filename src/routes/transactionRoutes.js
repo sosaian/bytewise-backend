7ab50f4645e70a62834c18de3f90b532703cdd65
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-07 01:23:18 -0300

    Modify budget-summary endpoint removing the need for id param
    Thanks to Auth implementation, this id lives inside the cookie sent with each request

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -9,6 +9,6 @@ router.get('/api/transaction/:id', authMiddleware, TransactionController.get)
 router.put('/api/transaction/:id', authMiddleware, TransactionController.update)
 router.delete('/api/transaction/:id', authMiddleware, TransactionController.delete)
 
-router.get('api/budget-summary/:id_user', authMiddleware, TransactionController.getBudgetSummary)
+router.get('/api/budget-summary', authMiddleware, TransactionController.getBudgetSummary)
 
 export default router
