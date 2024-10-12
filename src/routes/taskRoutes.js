e9e2846ec4866e368094367af120c07a01dd57e0
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-12 16:25:47 -0300

    Add /api/task/user endpoint for TaskController.getAll()
    Sidenote: in expressJS, the router needs the most specific routes to be defined first, so each route can handle every more specific routes that are sent in that endpoint like GET params

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -4,9 +4,12 @@ import { TaskController } from '../controller/taskController.js'
 
 const router = express.Router()
 
+router.get('/api/task/user', authMiddleware, TaskController.getAll)
+
 router.post('/api/task', authMiddleware, TaskController.create)
 router.get('/api/task/:id', authMiddleware, TaskController.get)
 router.put('/api/task/:id', authMiddleware, TaskController.update)
 router.delete('/api/task/:id', authMiddleware, TaskController.delete)
+
 
 export default router
