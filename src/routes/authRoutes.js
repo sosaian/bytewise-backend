dcccdb6b918da643592615d39de46b25fca704e8
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-13 18:38:56 -0300

    Add /auth/check GET endpoint for frontend's login verification

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router()
 router.post('/auth/register', AuthController.register)
 router.post('/auth/login', AuthController.login)
 router.post('/auth/logout', AuthController.logout)
+router.get('/auth/check', AuthController.checkLogin)
 
 export default router
